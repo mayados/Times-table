07386082bd6cddb50aecf87e7a4062a9cb527346
mayados <mariannedos.cours@gmail.com>
2024-05-07 13:28:02 +0200

    Add try and catch + for loop

--- Times table/Program.cs
+++ Times table/Program.cs
@@ -11,7 +11,24 @@ namespace Times_table
         static void Main(string[] args)
         {
 
-            
+            Console.Write("Entrez un nombre pour connaître sa table de multiplication : ");
+
+            try
+            {
+                int numInput = Convert.ToInt32(Console.ReadLine());
+
+                for(int i = 1; i <= 10; i++) {
+                    int result = i * numInput;
+                    Console.WriteLine($"{i} * {numInput} = {result}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message );
+            }
+
+
+            Console.ReadLine();
 
         }
     }
